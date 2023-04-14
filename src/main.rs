f1813f06e246de5141ad2a3d730714af5b8caded
Collin Styles <collingstyles@gmail.com>
2023-04-14 15:43:54 -0700

    Read a string from stdin and return its UUIDv5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::Read;
+use uuid::Uuid;
+
 fn main() {
-    println!("Hello, world!");
+    let namespace = Uuid::NAMESPACE_OID;
+    let message = read_from_stdin();
+    let message = message.trim_end();
+    let uuid = Uuid::new_v5(&namespace, message.as_bytes());
+    println!("{uuid}");
+}
+
+fn read_from_stdin() -> String {
+    let mut buffer = String::new();
+    std::io::stdin().read_to_string(&mut buffer).unwrap();
+
+    buffer
 }
