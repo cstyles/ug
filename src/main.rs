a52fa8ad4a326aad65e0e6211faf7d93eded58f9
Collin Styles <collingstyles@gmail.com>
2023-04-14 21:39:27 -0700

    Add -l and -U options to allow specifying case of output

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use std::io::Read;
 use uuid::Uuid;
 
+enum Case {
+    Lowercase,
+    Uppercase,
+}
+
 fn main() {
+    let case = parse_args();
+
     let namespace = Uuid::NAMESPACE_OID;
     let message = read_from_stdin();
     let message = message.trim_end();
     let uuid = Uuid::new_v5(&namespace, message.as_bytes());
-    println!("{uuid}");
+
+    match case {
+        Case::Lowercase => println!("{uuid:x}"),
+        Case::Uppercase => println!("{uuid:X}"),
+    }
 }
 
 fn read_from_stdin() -> String {
@@ -19,4 +30,18 @@ fn read_from_stdin() -> String {
     std::io::stdin().read_to_string(&mut buffer).unwrap();
 
     buffer
+}
+
+fn parse_args() -> Case {
+    let mut case = Case::Lowercase;
+
+    for arg in std::env::args().skip(1) {
+        match arg.as_str() {
+            "-l" | "--lowercase" => case = Case::Lowercase,
+            "-U" | "--uppercase" => case = Case::Uppercase,
+            _ => {}
+        }
+    }
+
+    case
 }
